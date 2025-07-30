7ff6316915efd5e719a7423b18a7cc34d019437b
slavmin <slavmin@gmail.com>
2025-07-30 19:31:37 +0300

    first commit

--- app/src/api/profile.ts
+++ app/src/api/profile.ts
@@ -1,4 +1,4 @@
 import { createApiClient } from './generic'
 import type { OperatorProfile } from '@/types/profile'
 
-export const profileApi = createApiClient<Partner>('operator')
+export const profileApi = createApiClient<OperatorProfile>('operator')
