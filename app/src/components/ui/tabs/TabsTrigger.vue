2c602fde39aab0e707722e91dbebc956a505166f
slavmin <slavmin@gmail.com>
2025-07-30 18:02:04 +0300

    first commit

--- app/src/components/ui/tabs/TabsTrigger.vue
+++ app/src/components/ui/tabs/TabsTrigger.vue
@@ -17,8 +17,6 @@ const setActiveTab = inject<(value: string) => void>('setActiveTab')
     @click="() => setActiveTab?.(value)"
     class="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
     :class="{
-      // 'bg-background text-foreground shadow-sm': activeTab === value,
-      // 'hover:text-foreground': activeTab !== value,
       'bg-background text-emerald-500 shadow-sm': activeTab === value,
       'hover:text-emerald-500/80 text-muted-foreground': activeTab !== value,
     }"
