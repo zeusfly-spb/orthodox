304675b4d713c9980fe4597658a3498a265be3b6
slavmin <slavmin@gmail.com>
2025-08-02 18:29:59 +0300

    commit

--- app/src/components/ui/radio-group/RadioGroupItem.vue
+++ app/src/components/ui/radio-group/RadioGroupItem.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import type { RadioGroupItemProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
+import { reactiveOmit } from '@vueuse/core'
 import { CircleIcon } from 'lucide-vue-next'
 import { RadioGroupIndicator, RadioGroupItem, useForwardProps } from 'reka-ui'
-import { computed } from 'vue'
+import { cn } from '@/lib/utils'
 
 const props = defineProps<RadioGroupItemProps & { class?: HTMLAttributes['class'] }>()
 
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
-
-  return delegated
-})
+const delegatedProps = reactiveOmit(props, 'class')
 
 const forwardedProps = useForwardProps(delegatedProps)
 </script>
