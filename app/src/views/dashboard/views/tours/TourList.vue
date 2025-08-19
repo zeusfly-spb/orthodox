f5e336fb5a660678b8c49cba92285a2b6532c421
Роман Будко <romanjaneiro@gmail.com>
2025-08-19 12:50:51 +0300

    fix: сдвинул все табличные формы

--- app/src/views/dashboard/views/tours/TourList.vue
+++ app/src/views/dashboard/views/tours/TourList.vue
@@ -99,7 +99,7 @@ const handleEditTour = (id: string | number) => {
 </script>
 
 <template>
-  <div>
+  <div class="ml-70">
     <div class="flex flex-col gap-6 rounded-xl py-6 mb-8">
       <div class="flex shrink-0 items-center justify-between gap-2">
         <!-- Левая часть -->
